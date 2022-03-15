e52c139130fbf627bcc795e6596a2512a0571d2e
1Ru1 <rokaiamedhat1@gmail.com>
2022-03-15 14:17:18 +0200

    initial commit

---
+++ pizza.js
@@ -0,0 +1,16 @@
+const pizza = document.getElementById("pizza");
+const num =document.getElementById("num");
+var currImage = 1;
+var NumOfSllices =0;
+pizza.addEventListener("click", (e) => {
+    console.log("clicked!!");
+    if(currImage===4){
+        
+        currImage = 1;
+        num.innerHTML=++NumOfSllices;
+    } else currImage++;
+    pizza.src=`/img/${currImage}.svg`;
+})
+
+
+
