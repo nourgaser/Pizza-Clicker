15b185b229133850e29d3538b10caad4c2e433e1
nourgaser <nourgaser2012@gmail.com>
2022-11-23 22:49:18 +0200

    bug fix

--- scripts/pizza.js
+++ scripts/pizza.js
@@ -1,16 +1,14 @@
 const pizza = document.getElementById("pizza");
-const num =document.getElementById("num");
+const num = document.getElementById("num");
 var currImage = 1;
-var NumOfSllices =0;
+var NumOfSllices = 0;
 pizza.addEventListener("click", (e) => {
-    console.log("clicked!!");
-    if(currImage===4){
-        
-        currImage = 1;
-        num.innerHTML=++NumOfSllices;
-    } else currImage++;
-    pizza.src= `../img/${currImage}.svg`;
-})
-
-
-
+  console.log("clicked!!");
+  if (currImage === 4) {
+    currImage = 1;
+    num.innerHTML = ++NumOfSllices;
+  } else currImage++;
+  if (window.location.href.split("/").at(-1) !== "index.html")
+    pizza.src = `../img/${currImage}.svg`;
+  else pizza.src = `./img/${currImage}.svg`;
+});
